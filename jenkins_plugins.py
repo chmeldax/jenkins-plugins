14a584962079eb290c6de82460e372759d02903b
Jan Chmelíček <chmelicek@plutanium.cz>
2016-09-21 10:05:48 +0200

    Dependencies resolution

--- jenkins_plugins.py
+++ jenkins_plugins.py
@@ -1,7 +1,12 @@
 import requests
+import zipfile
+import re
+import os.path
+
+#PLUGIN_DIRECTORY = '/var/lib/jenkins'  # With paths
+PLUGIN_DIRECTORY = '/home/chmelda/jenkins'  # With paths
 
 
-PLUGIN_DIRECTORY = '/var/lib/jenkins'  # With paths
 CACHE = {}
 
 
@@ -11,14 +16,28 @@ class Plugin(object):
         self._name = name
 
     def download(self):
+        if self._plugin_exists:
+            return self
         response = requests.get(self._plugin_url)
+        self._save_plugin(response)
+        self._get_dependencies()
+
+    def _get_dependencies(self):
+        for dependency in Manifest(self._plugin_file).dependencies:
+            Plugin(dependency).download()
+
+    def _save_plugin(self, response):
         with open(self._plugin_file, 'wb') as file:
             file.write(response.content)
+        print('Downloaded plugin {plugin}'.format(plugin=self._name))
 
-    def get_dependencies(self):
-        # parse META-INF/MANIFEST.MF
-        # parse 'Plugin-Dependencies:'
-        pass
+    @property
+    def _plugin_exists(self):
+        if os.path.isfile(self._plugin_file):
+            print('Skipping plugin {plugin}, already installed.'.format(plugin=self._name))
+            return True
+        return False
+
 
     @property
     def _plugin_url(self):
@@ -28,3 +47,26 @@ class Plugin(object):
     @property
     def _plugin_file(self):
         return PLUGIN_DIRECTORY + '/{name}.hpi'.format(name=self._name)
+
+
+class Manifest(object):
+
+    def __init__(self, plugin_file):
+        self._plugin_file = plugin_file
+
+    @property
+    def dependencies(self):
+        for line in self._load_manifest():
+            regex = re.search(b"^Plugin-Dependencies: (.*)", line)
+            if regex:
+                return [x.split(':')[0] for x in regex.group(1).decode('ascii').split(',')]
+        return []
+
+    def _load_manifest(self):
+        with zipfile.ZipFile(self._plugin_file) as zip_file:
+             with zip_file.open('META-INF/MANIFEST.MF', 'r') as manifest_file:
+                 for line in manifest_file:
+                     yield line
+
+
+Plugin('kubernetes').download()
